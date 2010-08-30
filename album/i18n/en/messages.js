943aed67fc92330968a5188389b2badc92617107
Magnus Jönsson <magnus@exito.com>
2010-08-30 19:53:51 +0000

    help text for album

--- album/i18n/en/messages.js
+++ album/i18n/en/messages.js
@@ -45,9 +45,47 @@ messages = {
 	
 	/* Help messages */
 	"help-box-header" : "Bubba Album help",
-	"help-info::anon::main": "Click on login to login etc...",
-	"help-info::user::main": "Click on manager button to enter manager mode...",
-	"help-info::manager::main": "Add a new album by clicking on the add new album button...",
+	"help-info::anon::main": "
+	<h3>Viewing photos</h3>
+	<ul>	
+		<li>To enter an album double click it or click the arrow to the right on the page.</li>
+		<li>Click a picture to view it and click 'Slide show' for automatic picture browsing.</li>
+	</ul>
+	
+	<h3>Log in to...</h3>
+	<ul>
+		<li>View private photo albums.</li>
+		<li>Create photo albums.</li>
+		<li>Edit photo albums.</li>
+	</ul>
+	
+	<p>B3 users (not administrator) may log in and create albums and create album users. Album users may log in and view albums.</p>
+	",
+	
+	"help-info::user::main": "
+	<h3>Viewing photos</h3>
+	<ul>
+		<li>To enter an album double click it or click the arrow to the right on the page.</li>
+		<li>Click a picture to view it and click 'Slide show' for automatic picture browsing.</li>
+	</ul>
+	
+	<h3>Managing photos</h3>
+	<p>Click on the button with a wrench symbol to enter 'Manager mode'. You have to be logged in as your standard B3 user (not as administrator) to add and edit albums.</p>
+	",
+	
+	"help-info::manager::main": "
+	<h3>Menu items</h3>
+	<p>Observe that you have to highlight an album or a photo for some menu items to be selectable.</p>
+	<ul>
+		<li><strong>Create album</strong> - will add a new album. Before creating an album your photos must be located in your storage\pictures catalog.</li>
+		<li><strong>Add images</strong> - will add images into the album you are currantly located in.</li>
+		<li><strong>Move</strong> - will move an item. Highlight one or more items, click Move, browse to the desired destination and confirm the move with the Yes button.</li>
+		<li><strong>Rename</strong> - will rename or edit the description for an item.</li>
+		<li><strong>Permissions</strong> - will edit album permissions. You may edit permissions for albums recursively (subalbums will gain the same permissions as the selected album).</li>
+		<li><strong>Manage users</strong> - add/edit album users that only has permission to view albums (not log into B3 itself).</li>
+		<li><strong>Delete</strong> - delete albums or photos.</li>	
+	</ul>
+	",
 
 	/* Create wizard */
 	"albummanager-create-button-finish":"Create album"
