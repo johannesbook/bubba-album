fd3d9214a8b61a5177d5a3d162dcce58dfff0031
Carl Fürstenberg <carl@excito.com>
2011-05-20 14:58:23 +0200

    Adding Spanish user contributed translation

---
+++ album/i18n/es/messages.js
@@ -0,0 +1,95 @@
+messages = {
+	'next': 'Siguiente',
+	'back': 'Anterior',
+	'ui-albummanager-album-name' : "&Aacute;lbum ",
+	'ui-albummanager-album-caption' : "Descripci&oacute;n",
+	'ui-albummangaer-images-in-album' : "en",
+	/* Topnav */
+	"topnav-authorized-bubba" : "Conectado como " + config.name + " usuario '%s'",
+	"topnav-authorized-album" : "Viendo &aacute;lbum como '%s'",
+	"topnav-authorized" : "Conectado como '%s'",
+	"topnav-not-authorized" : "Viendo &aacute;lbum an&oacute;nimamente",
+	"topnav-login" : "Conectar",
+	"topnav-logout" : "Desconectar",
+	/* Log out */
+	"logout-dialog-title" : "Seguir con desconexi&oacute;n?",
+	"logout-dialog-message" : "",
+	"logout-dialog-button-logout" : "Desconexi&oacute;n",
+
+	/* Log in */
+	"login-dialog-continue" : "Conexión",
+
+	"albummanager-move-notice": "Selecciona destino para mover %d &aacute;lbumes y %d im&aacute;genes?",
+	"albummanager-move-no": "No",
+	"albummanager-move-yes": "Si",
+
+	"albummanager-delete-dialog-button-label": "Borrar",
+	"albummanager-create-dialog-button-label": "Crear",
+	"albummanager-perm-dialog-button-label": "Actualizar",
+	"albummanager-modify-dialog-button-label": "Actualizar",
+	"albummanager-add-dialog-button-label": "A&ntilde;adir",
+
+	"album-image-count": "%d im&aacute;genes",
+	"album-image-subalbum-count": "%d im&aacute;genes y %d sub-&aacute;lbumes",
+
+	/* Permissions */
+	"albummanager-label-public": "&iquest;Permitir acceso an&oacute;nimo?",
+	"albummanager-label-recursive": "&iquest;Aplicar permisos recursivamente?",
+	"album-users-entry":"%s (%s)",
+	"album-users-delete-message": "Borrar usuario <strong>%s</strong> (\"%s\")?",
+
+	/* User manager */
+	"albummanager-del-no": "Cancelar",
+	"albummanager-del-yes": "Borrar usuario",
+	"albummanager-users-add": "A&ntilde;adir usuario",
+	"albummanager-button-edit": "Editar seleccionado",
+
+
+	/* Help messages */
+	"help-box-header" : config.name + " Ayuda de &Aacute;lbum",
+	"help-info::anon::main":
+	"<h3>Viendo fotos</h3>"
+	+"<ul>"
+		+"<li>Para acceder a un &aacute;lbum hacerle doble click o seleccionar la flecha de la derecha.</li>"
+		+"<li>Selecciona una foto para verla y selecciona 'Slide show' para visionarlo autom&aacute;ticamente.</li>"
+	+"</ul>"
+
+	+"<h3>Conectar a...</h3>"
+	+"<ul>"
+		+"<li>Ver &aacute;lbumes de fotos privados.</li>"
+		+"<li>Crear &aacute;lbumes de fotos.</li>"
+		+"<li>Editar &aacute;lbumes de fotos.</li>"
+	+"</ul>"
+
+	+"<p> Los usuarios de " + config.name + "(no el administrator) pueden conectarse y crear &aacute;lbumes y usuarios. Estos usuarios pueden conectarse y ver los &aacute;lbumes.</p>"
+	,
+
+	"help-info::user::main":
+	"<h3>Viendo fotos</h3>"
+	+"<ul>"
+		+"<li>Para acceder a un &aacute;lbum hacerle doble click o seleccionar la flecha de la derecha.</li>"
+		+"<li>Selecciona una foto para verla y selecciona 'Slide show' para visionarlo autom&aacute;ticamente.</li>"
+	+"</ul>"
+
+	+"<h3>Gestionar fotos</h3>"
+	+"<p>Selecciona el bot&oacute;n con forma de engranaje para entrar en 'Modo gesti&oacute;n'. Tienes que estar conectado con tu usuario B3 (no como administrador) para a&ntilde;adir y editar &aacute;lbumes.</p>"
+	,
+
+	"help-info::manager::main":
+	"<h3>Elementos de Men&uacute;</h3>"
+	+"<p>Observa que tienes que seleccionar un &aacute;lbum o una foto para que se activen ciertos elementos del men&uacute;.</p>"
+	+"<ul>"
+		+"<li><strong>Crear &aacute;lbum</strong> - A&ntilde;adir un nuevo &aacute;lbum. Antes de crear un &aacute;lbum tus fotos deben estar en tu cat&aacute;logo 'storage/pictures'.</li>"
+		+"<li><strong>A&ntilde;adir im&aacute;genes</strong> - A&ntilde;ade im&aacute;genes al &aacute;lbum en el que est&aacute;s posicionado.</li>"
+		+"<li><strong>Mover</strong> - Mueve un &aacute;lbum o fotos. Resalta uno o m&aacute;s elementos, pulsa Mover, navega hasta el destino deseado y confirma con el bot&oacute;n de aceptar.</li>"
+		+"<li><strong>Renombrar</strong> - Cambia el nombre o edita la descripci&oacute;n de un elemento.</li>"
+		+"<li><strong>Permisos</strong> - Edita los permisos del &aacute;lbum. Decide quien debe ser capaz de ver tus &aacute;lbumes de fotos. Establece diferentes permisos para diferentes &aacute;lbumes. Para que un &aacute;lbum sea accesible por cualquiera, elige 'Permitir acceso an&oacute;nimo'. Puedes editar los permisos de un &aacute;lbum recursivamente (los sub &aacute;lbumes heredan los mismos permisos que el &aacute;lbum seleccionado).</li>"
+		+"<li><strong>Gesti&oacute;n de usuarios</strong> - Crear y editar 'usuarios de &aacute;lbum'. 'usuarios de &aacute;lbum' solo tienen permiso para ver &aacute;lbumes, no pueden conectarse al B3. Truco! Crea diferentes 'usuarios de &aacute;lbum' con acceso a diferentes &aacute;lbumes...</li>"
+		+"<li><strong>Eliminar</strong> - Borrar &aacute;lbumes o fotos. Las fotos no ser&aacute;n borradas del B3, solamente del &aacute;lbum.</li>"
+	+"</ul>"
+	,
+
+	/* Create wizard */
+	"albummanager-create-button-finish":"Crear &aacute;lbum"
+
+};
